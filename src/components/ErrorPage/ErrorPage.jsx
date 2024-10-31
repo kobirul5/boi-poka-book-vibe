45f7375947e32abe88a8bb7ee45bab2dd5af2db5
kobirul5 <kobirul05j@gmail.com>
2024-11-01 01:26:43 +0600

    added home,footer, dashboard,navbar,error page

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,10 @@
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1 className="text-5xl text-center">This Page Not Found</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-5xl">I am from Home</h1>
+        </div>
+    );
+};
+
+export default Home;
