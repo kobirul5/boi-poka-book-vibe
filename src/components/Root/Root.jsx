afd9298e8e6cb1c9b922a2051974247218dd00e6
kobirul5 <kobirul05j@gmail.com>
2024-11-01 00:57:10 +0600

    set up project for devlopment site

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Root = () => {
+    return (
+        <div>
+            <h1 className='text-red-300'>welcome from root</h1>
+        </div>
+    );
+};
+
+export default Root;
